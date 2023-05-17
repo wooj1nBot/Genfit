1eace2bbcecad4fb651b5abe5616c5c3d24bcbb2
sangpil0104 <psp0104@naver.com>
2023-05-17 20:05:17 +0900

    상필 intro2

---
+++ app/src/main/java/com/realese/IntroSecond_Activity2.java
@@ -0,0 +1,14 @@
+package com.realese;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroSecond_Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_second2);
+    }
+}
