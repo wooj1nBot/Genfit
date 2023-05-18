3e45e9603e3da73dbb0ef8101aae978db33e49b2
eahc00 <eahc00@naver.com>
2023-05-18 11:27:25 +0900

    ㅎㅅㅎ

--- app/src/main/java/com/realese/Preset/PresetGenderActivity.java
+++ app/src/main/java/com/realese/Preset/PresetGenderActivity.java
@@ -3,14 +3,20 @@ package com.realese.Preset;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.realese.R;
 
 public class PresetGenderActivity extends AppCompatActivity {
 
+    Button man_btn;
+    Button woman_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preset_gender);
+
+
     }
 }
