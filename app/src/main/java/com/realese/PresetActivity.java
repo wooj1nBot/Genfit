bc2fb4015bc3d24ae7f26b5e9756e91f33355e7b
eahc00 <eahc00@naver.com>
2023-05-18 10:22:17 +0900

    프리셋

--- app/src/main/java/com/realese/PresetActivity.java
+++ app/src/main/java/com/realese/PresetActivity.java
@@ -3,12 +3,46 @@ package com.realese;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PresetActivity extends AppCompatActivity {
+
+    ImageView back_btn;
+    Button finish_btn;
+    TextView next_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preset);
+
+        back_btn = findViewById(R.id.bback);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        finish_btn = findViewById(R.id.finish);
+        finish_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        next_btn = findViewById(R.id.next);
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
