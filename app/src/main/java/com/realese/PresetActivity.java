51df3f64c311e3c79fd05f7bd1349a8cf027cee5
eahc00 <eahc00@naver.com>
2023-05-18 10:10:02 +0900

    프리셋 UI

---
+++ app/src/main/java/com/realese/PresetActivity.java
@@ -0,0 +1,14 @@
+package com.realese;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PresetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preset);
+    }
+}
