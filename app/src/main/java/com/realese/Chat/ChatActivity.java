e40a43f497ffdca53afea85940c9acf474d17774
kmmygyum <small0753@daum.net>
2023-05-19 16:17:42 +0900

    .

--- app/src/main/java/com/realese/Chat/ChatActivity.java
+++ app/src/main/java/com/realese/Chat/ChatActivity.java
@@ -1,16 +1,33 @@
 package com.realese.Chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.realese.Frags.FragMain;
 import com.realese.R;
 
 public class ChatActivity extends AppCompatActivity {
+
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FragMain.class));
+                finish();
+            }
+        });
     }
 }
